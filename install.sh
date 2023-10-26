722e5bc180f28bb11b9824908b4dce97ee8356c5
LFdev <lfdev+gitlab@envs.net>
2023-10-25 23:11:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ is_app_installed() {
     type "$1" &>/dev/null
 }
 
+INSTALL_DIR="${HOME}/.config"
+PLUGINS_DIR="${INSTALL_DIR}/tmux/plugins"
+
 REPODIR="$(
     cd "$(dirname "$0")"
     pwd -P
@@ -15,32 +18,40 @@ REPODIR="$(
 cd "$REPODIR"
 
 if ! is_app_installed tmux; then
-    printf "WARNING: \"tmux\" command is not found. \
-        Install it first\n"
+    printf "ERROR: \"tmux\" not found! \
+        Please install it first\n"
     exit 1
 fi
 
-if [ ! -e "$XDG_DATA_HOME/tmux/plugins/tpm" ]; then
+mkdir -p "${PLUGINS_DIR}"
+
+if [ ! -e "${PLUGINS_DIR}/tpm" ]; then
     printf "WARNING: Cannot find TPM (Tmux Plugin Manager) \
-        at default location: \$XDG_DATA_HOME/tmux/plugins/tpm.\n"
-    git clone https://github.com/tmux-plugins/tpm $XDG_DATA_HOME/tmux/plugins/tpm
+        at default location: ${PLUGINS_DIR}/tpm.\n"
+    git clone https://github.com/tmux-plugins/tpm "${PLUGINS_DIR}/tpm"
 fi
 
-# TODO: Add check to mitigate two tmux configs
-if [ -e "$HOME/.tmux.conf" ]; then
-    printf "Found existing .tmux.conf in your \$XGD_CONFIG_HOME directory.\
-        Will create a backup at $XDG_CONFIG_HOME/tmux/tmux.conf.bak\n"
-    cp -f "$HOME/.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf.bak" 2>/dev/null || true
+if [ -e "${HOME}/.tmux.conf" ]; then
+    printf "WARNING: Found existing .tmux.conf file in \$HOME directory.\
+        It will be moved to ${INSTALL_DIR}/tmux/tmux.conf.home\n"
+    mv "${HOME}/.tmux.conf" "${INSTALL_DIR}/tmux/tmux.conf.home" 2>/dev/null || true
 fi
 
-stow -vt ~ tmux
+if [ -e "${INSTALL_DIR}/tmux/tmux.conf" ]; then
+    printf "WARNING: Found existing tmux.conf file in ${INSTALL_DIR}/tmux directory.\
+        It will be moved to ${INSTALL_DIR}/tmux/tmux.conf.config\n"
+    mv "${INSTALL_DIR}/tmux/tmux.conf" "${INSTALL_DIR}/tmux/tmux.conf.config" 2>/dev/null || true
+fi
+
+cp -a ./tmux/* "${INSTALL_DIR}"/tmux/
 
 # Install TPM plugins.
 # TPM requires running tmux server, as soon as `tmux start-server` does not work
 # create dump __noop session in detached mode, and kill it when plugins are installed
 printf "Install TPM plugins\n"
 tmux new -d -s __noop >/dev/null 2>&1 || true
-"$XDG_DATA_HOME"/tmux/plugins/tpm/bin/install_plugins || true
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "${PLUGINS_DIR}"
+"${PLUGINS_DIR}"/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
 
 printf "OK: Completed\n"
