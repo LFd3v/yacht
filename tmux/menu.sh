41ea52ba9734904a8266346d49b02c6ad175bded
LFdev <lfdev+gitlab@envs.net>
2023-10-30 23:31:07 -0300

    Add config for default layout, update support files

--- tmux/menu.sh
+++ tmux/menu.sh
@@ -2,18 +2,25 @@
 
 DATE="$(date)"
 SESSIONS="$(tmux ls | wc -l)"
+CS=$(~/.config/tmux/plugins/tmux-continuum/scripts/continuum_status.sh)
+[ "$CS" == "off" ] && CI="15" || CI="0"
+[ "$CS" == "off" ] && CM="" || CI="min"
+
 tmux display-menu -T "#[align=centre]Status" -x P -y P \
     "" \
     "-#[nodim]$DATE" "" {} \
-    "-#[nodim]Mouse: #{?mouse,on,Off}" "" {} \
+    "-#[nodim]#[align=centre]tmux #{version}" "" {} \
+    "" \
+    "-#[nodim]Continuum: ${CS}${CM}" "" {} \
+    "-#[nodim]Mouse: #{?mouse,on,off}" "" {} \
     "-#[nodim]Session windows: #{session_windows}" "" {} \
     "-#[nodim]Sessions: $SESSIONS" "" {} \
-    "-#[nodim]Version: #{version}" "" {} \
     "" \
     "Choose tree" c "choose-tree -Z" \
     "Detach" d "confirm-before -p 'detach from session #S? (y/N)' detach" \
     "display Time" t "run -b -d 0.2 'tmux clock-mode'" \
     "display Buffers" b "choose-buffer -Z" \
+    "toggle continUum" u "set -g @continuum-save-interval $CI" \
     "toggle Mouse" m "setw mouse" \
     "" \
     "Quit" q "confirm-before -p 'kill-session #S? (y/N)' kill-session"
--- tmux/startup.sh
+++ tmux/startup.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
-tmux select-layout main-vertical
-tmux set -gw main-pane-width 60
+tmux select-layout $DEFAULT_LAYOUT
+tmux set -gw main-pane-width $DEFAULT_HW
+tmux set -gw main-pane-height $DEFAULT_HW
 
 DATE="$(date)"
 SESSIONS="$(tmux ls | wc -l)"
+CS=$(~/.config/tmux/plugins/tmux-continuum/scripts/continuum_status.sh)
+CS=$(~/.config/tmux/plugins/tmux-continuum/scripts/continuum_status.sh)
+[ "$CS" == "off" ] && CI="15" || CI="0"
+[ "$CS" == "off" ] && CM="" || CI="min"
+
 tmux bind -T root MouseDown3StatusRight display-menu -O -T "#[align=centre]Status" -t = -x M -y S \
     "" \
     "-#[nodim]$DATE" "" {} \
-    "-#[nodim]Mouse: #{?mouse,on,Off}" "" {} \
+    "-#[nodim]#[align=centre]tmux #{version}" "" {} \
+    "" \
+    "-#[nodim]Continuum: ${CS}${CM}" "" {} \
+    "-#[nodim]Mouse: #{?mouse,on,off}" "" {} \
     "-#[nodim]Session windows: #{session_windows}" "" {} \
     "-#[nodim]Sessions: $SESSIONS" "" {} \
-    "-#[nodim]Version: #{version}" "" {} \
     "" \
     "Choose tree" c "choose-tree -Z" \
     "Detach" d "confirm-before -p 'detach from session #S? (y/N)' detach" \
     "display Time" t "run -b -d 0.2 'tmux clock-mode'" \
     "display Buffers" b "choose-buffer -Z" \
+    "toggle continUum" u "set -g @continuum-save-interval $CI" \
     "toggle Mouse" m "setw mouse" \
     "" \
     "Quit" q "confirm-before -p 'kill-session #S? (y/N)' kill-session"
